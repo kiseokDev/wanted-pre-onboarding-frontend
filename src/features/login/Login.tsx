e018392cee53f18bb6fd7b6c43ff71a0dd78958e
kiseokDev <dev.leekiseok@gmail.com>
2023-08-02 17:33:54 +0900

    환경설정 완료

---
+++ src/features/login/Login.tsx
@@ -0,0 +1,10 @@
+export default function Login() {
+    ///signup 경로에 회원가입 기능을 개발해주세요
+
+    return <div>
+        <input data-testid="email-input" />
+        <input data-testid="password-input" />
+        <button data-testid="signup-button">회원가입</button>
+    </div>;
+
+}
