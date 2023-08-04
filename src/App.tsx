43eb22adec0c0b469c92548fcc18e0858639fcc1
kiseokDev <dev.leekiseok@gmail.com>
2023-08-04 12:20:42 +0900

    로그인 여부 리다이렉션 분기 완료

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import { SignUp, SignIn, Home, Todo } from './pages/index';
+import { SignUp, SignIn, Todo } from './pages/index';
 
 
 
 function App() {
+  const token = localStorage.getItem('jwt');
+  console.log(token);
+
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Navigate to="/signin" />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/todo" element={<Todo />} />
+        <Route path="/signin" element={token ? <Navigate to="/todo" /> : <SignIn />} />
+        <Route path="/signup" element={token ? <Navigate to="/todo" /> : <SignUp />} />
+        <Route path="/todo" element={!token ? <Navigate to="/signin" /> : <Todo />} />
       </Routes>
     </Router>
   );
