f39d950ed4dc762a6a90444cb8dfa7c762e3874e
kiseokDev <dev.leekiseok@gmail.com>
2023-08-11 12:10:14 +0900

    투두테스트 시작 전

---
+++ src/__test__/todo/Todo.test.tsx
@@ -0,0 +1,8 @@
+import { render } from "@testing-library/react";
+import { TodoList } from "../../components";
+import TodoInsert from "../../components/todo/TodoInsert";
+
+describe("Todo 컴포넌트 테스트", () => {
+    it("/todo경로에 접속하면 투두 리스트의 아이템이 화면에 보여진다", () => {
+    });
+});
