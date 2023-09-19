7899c4feca472826058343d5acfc8d7a92b579bd
kiseokDev <dev.leekiseok@gmail.com>
2023-09-19 17:49:45 +0900

    FIX: useAuth 리액트쿼리 코드삭제

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -3,10 +3,7 @@ import {validateForm} from "../features";
 import {AuthAPI} from "../api";
 import {useNavigate} from "react-router-dom";
 import {AuthContext} from "../components";
-import {AxiosError} from "axios";
-import {useMutation} from "react-query";
-import {AuthPayload} from "../types";
-import {hasMessage, isErrorMessage} from "../util";
+import axios from "axios";
 
 export default function useAuthApiHandlersHook() {
   const api = new AuthAPI();
@@ -28,88 +25,47 @@ export default function useAuthApiHandlersHook() {
     setIsFormValid(validateForm(email, newPassword));
   };
 
-  // const handleSignUpSubmit = async (e: React.FormEvent) => {
-  //   e.preventDefault();
-
-  //   try {
-  //     const response = await api.signUpApi({email, password});
-  //     if (response.status === 201) {
-  //       navigator("/signin");
-  //     }
-  //   } catch (error) {
-  //     if (axios.isAxiosError(error)) {
-  //       if (error.response?.status === 400) {
-  //         // 동일한 메일 존재시 alert
-  //         alert(error.response.data.message);
-  //       }
-  //     }
-  //   }
-  // };
-
-  // const handleSignInSubmit = async (e: React.FormEvent) => {
-  //   e.preventDefault();
-  //   try {
-  //     const response = await api.signInApi({email, password});
-  //     if (response.status === 200) {
-  //       localStorage.setItem("access_token", response.data.access_token); // JWT를 로컬 스토리지에 저장
-  //       setToken(response.data.access_token); // 토큰 설정
-  //       navigator("/todo");
-  //     }
-  //   } catch (error) {
-  //     if (axios.isAxiosError(error)) {
-  //       if (error.response?.status === 401) {
-  //         // 비밀번호 오류
-  //         alert("이메일 혹은 비밀번호 오류");
-  //       } else if (error.response?.status === 404 && error.response.data.message === "해당 사용자가 존재하지 않습니다.") {
-  //         // alert("해당 사용자가 존재하지 않습니다.");
-  //         alert("이메일 혹은 비밀번호 오류");
-  //       }
-  //     }
-  //   }
-  // };
-
-  const signUpMutation = useMutation((payload: AuthPayload) => api.signUpApi(payload), {
-    onSuccess: (data) => {
-      // Handle success state here...
-      if (data.status === 201) {
-        navigator("/signin");
-      }
-    },
-    onError: (error: AxiosError) => {
-      if (error.response?.status === 400 && isErrorMessage(error.response.data)) {
-        alert(error.response.data.message);
-      }
-    },
-  });
-
-  const signInMutation = useMutation((payload: AuthPayload) => api.signInApi(payload), {
-    onSuccess: (data) => {
-      // Handle success state here...
-      if (data.status === 200) {
-        localStorage.setItem("access_token", data.data.access_token);
-        setToken(data.data.access_token);
-        navigator("/todo");
-      }
-    },
-    onError: (error: AxiosError) => {
-      if (
-        error.response?.status === 401 ||
-        (error.response?.status === 404 && hasMessage(error.response.data) && error.response.data.message === "해당 사용자가 존재하지 않습니다.")
-      ) {
-        alert("이메일 혹은 비밀번호 오류");
-      }
-    },
-  });
 
   const handleSignUpSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    signUpMutation.mutate({email, password});
+
+    try {
+      const response = await api.signUpApi({email, password});
+      if (response.status === 201) {
+        navigator("/signin");
+      }
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        if (error.response?.status === 400) {
+          alert(error.response.data.message);
+        }
+      }
+    }
   };
 
   const handleSignInSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    signInMutation.mutate({email, password});
+    try {
+      const response = await api.signInApi({email, password});
+      if (response.status === 200) {
+        localStorage.setItem("access_token", response.data.access_token); // JWT를 로컬 스토리지에 저장
+        setToken(response.data.access_token); // 토큰 설정
+        navigator("/todo");
+      }
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        if (error.response?.status === 401) {
+          // 비밀번호 오류
+          alert("이메일 혹은 비밀번호 오류");
+        } else if (error.response?.status === 404 && error.response.data.message === "해당 사용자가 존재하지 않습니다.") {
+          // alert("해당 사용자가 존재하지 않습니다.");
+          alert("이메일 혹은 비밀번호 오류");
+        }
+      }
+    }
   };
+
+ 
 
   return {email, password, isFormValid, handleEmailChange, handlePasswordChange, handleSignInSubmit, handleSignUpSubmit};
 }
